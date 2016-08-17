fc64837182f9ba22bde07fd6b02c43c0890724c2
Stalinvalarezo <stamluc97@hotmail.com>
2016-08-17 16:10:05 -0500

    Miklvndfdxfk

---
+++ app/src/main/java/com/facci/myapplication/MainActivity.java
@@ -0,0 +1,18 @@
+package com.facci.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void Click (View v){
+        Toast.makeText(MainActivity.this,"Hola mundo",Toast.LENGTH_SHORT).show();
+    }
+}
